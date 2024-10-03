5469eb842b3884b526713e2fa702666137a22a34
VERTAFORE\kashyaar <akashyap@vertafore.com>
2024-10-03 17:06:11 +0530

    Added vue parsing

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+
+
+export function add(a, b) {
+    return a + b;
+}
+
+export function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+// Example usage (you can remove this later)
+console.log(add(2, 3)); // 5
+console.log(greet('World')); // Hello, World!
