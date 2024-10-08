e23bcf08cf14ca2549a15786117649b6ec30cc8b
VERTAFORE\kashyaar <akashyap@vertafore.com>
2024-10-08 12:23:37 +0530

    Updated dashboard app view

--- resources/views/web/users/dashboard.blade.php
+++ resources/views/web/users/dashboard.blade.php
@@ -19,18 +19,31 @@
                 </div>
             </div>
             @else
-            	<div class="p-3 bg-success bg-opacity-10">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div>
-                        You have {{ auth()->user()->apps->count() }} associated active apps. You can view and manage them here.
+            <p class="fw-bold mb-4 text-success border-bottom border-2 d-inline-block p-2 pb-1">Your active apps</p>
+            @foreach(auth()->user()->apps as $app)
+            <div class="p-3 mb-2 border bg-opacity-10">
+                <div class="row align-items-center">
+                    <div class="col-md-6">
+                        <i class="bi bi-check-circle text-success"></i> <strong class="">{{ $app->name }}</strong> : <small>{{ $app->url }}</small>
                     </div>
-                    <div>
-                        <a href="{{ route('app.list') }}" class="btn btn-success float-end">Manage Apps</a>
+                    <div class="col-md-6 text-end">
+                        <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
+                            <button type="button" class="btn btn-light border">Feature Reviews</button>
+                            <button type="button" class="btn btn-light border">Surveys</button>
+                            <div class="btn-group" role="group">
+                                <button type="button" class="btn btn-light border" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="bi bi-three-dots-vertical"></i>
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="{{ route('app.list') }}">Manage App</a></li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+            @endforeach
             @endif
-
         </div>
     </div>
 </div>
