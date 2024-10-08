6fda9d53136e511a958f3a4cc1a5939672e67f16
ananthsa <sanantham@vertafore.com>
2024-10-08 11:17:51 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,10 @@ Route::get('about-us', [HomeController::class, 'about'])->name('about');
 
 Route::middleware('guest')->group(function() {
     Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
-    // Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('login', [AuthController::class, 'login'])->name('login');
 
     Route::get('register', [AuthController::class, 'showRegisterForm'])->name('register');
-    // Route::post('register', [AuthController::class, 'register'])->name('register');
+    Route::post('register', [AuthController::class, 'register'])->name('register');
 });
 Route::get('logout', [AuthController::class, 'logout'])->name('auth.logout');
 
